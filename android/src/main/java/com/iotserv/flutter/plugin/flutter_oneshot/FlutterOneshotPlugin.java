41ff37fb77f27c5edbc52321b769cd42580d0399
Farry <yu@iotserv.com>
2019-08-07 22:10:35 +0800

    init plugin

---
+++ android/src/main/java/com/iotserv/flutter/plugin/flutter_oneshot/FlutterOneshotPlugin.java
@@ -0,0 +1,25 @@
+package com.iotserv.flutter.plugin.flutter_oneshot;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterOneshotPlugin */
+public class FlutterOneshotPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_oneshot");
+    channel.setMethodCallHandler(new FlutterOneshotPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/flutter_oneshot.podspec
@@ -0,0 +1,21 @@
+#
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+Pod::Spec.new do |s|
+  s.name             = 'flutter_oneshot'
+  s.version          = '0.0.1'
+  s.summary          = 'OneShot for w60x wifi config'
+  s.description      = <<-DESC
+OneShot for w60x wifi config
+                       DESC
+  s.homepage         = 'http://example.com'
+  s.license          = { :file => '../LICENSE' }
+  s.author           = { 'Your Company' => 'email@example.com' }
+  s.source           = { :path => '.' }
+  s.source_files = 'Classes/**/*'
+  s.public_header_files = 'Classes/**/*.h'
+  s.dependency 'Flutter'
+
+  s.ios.deployment_target = '8.0'
+end
+
